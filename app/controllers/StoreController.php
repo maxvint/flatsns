ed04a2e172df768084d5dc852f3e2039b981df07
yuwenhui <yuwenhui1986@163.com>
2014-01-08 23:39:58 +0800

    modify store item

--- app/controllers/StoreController.php
+++ app/controllers/StoreController.php
@@ -2,14 +2,12 @@
 
 class StoreController extends BaseController {
 
-<<<<<<< HEAD
+
 	public function __construct(Store $store, Item $item)
 	{
 
 	}
 
-=======
->>>>>>> a8102a6a0aaba109521e4721c02b44c435f0862e
 	/**
 	 * Display a listing of the resource.
 	 *
@@ -17,7 +15,6 @@ class StoreController extends BaseController {
 	 */
 	public function getIndex()
 	{
-<<<<<<< HEAD
 		$items = Item::all();
 		return View::make('store.index', compact('items'));
 	}
@@ -31,9 +28,6 @@ class StoreController extends BaseController {
 	{
 		$item = Item::find($id);
 		return View::make('store.item', compact('item'));
-=======
-		return View::make('store.index');
->>>>>>> a8102a6a0aaba109521e4721c02b44c435f0862e
 	}
 
 
--- app/views/store/index.blade.php
+++ app/views/store/index.blade.php
@@ -1,19 +1,16 @@
 @extends('layout.application')
 
 @section('content')
-<<<<<<< HEAD
 <script language="javascript">
 	seajs.use('app/main', function(main) {
 		main.load('store');
 	});
 </script>
-=======
->>>>>>> a8102a6a0aaba109521e4721c02b44c435f0862e
+
 <div class="container main">
 	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12 store-list">
 			<ul id="goods" class="list-unstyled goods-list">
-<<<<<<< HEAD
 				@foreach($items as $item)
 				<li class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
 					<div class="item">
@@ -22,16 +19,6 @@
 						</div>
 						<div class="info">
 							<h5><a href="" data-toggle="tooltip" title="{{ $item->title }}">{{ $item->title }}</a></h5>
-=======
-				@for($i=1; $i<=12; $i++)
-				<li class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
-					<div class="item">
-						<div class="img">
-							<a href="{{ URL::to('goods/show/') }}"><img src="{{ asset('img/test/'.$i.'.jpg') }}" class="img-responsive"></a>
-						</div>
-						<div class="info">
-							<h5><a href="">Pebble Steel - 接近完美的智能手表</a></h5>
->>>>>>> a8102a6a0aaba109521e4721c02b44c435f0862e
 							<p>
 								<span><i class="like glyphicon glyphicon-heart"></i> 16</span>
 								<span>¥ 1,249.00</span>
@@ -39,11 +26,7 @@
 						</div>
 					</div>
 				</li>
-<<<<<<< HEAD
 				@endforeach
-=======
-				@endfor
->>>>>>> a8102a6a0aaba109521e4721c02b44c435f0862e
 			</ul>
 		</div>
 	</div>
